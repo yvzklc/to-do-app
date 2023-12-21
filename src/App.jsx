a735d7ba308858d20bbe07eb6f83cde23a47fe20
yvzklc <kilciyavuz34@gmail.com>
2023-12-21 14:20:44 +0300

    to do function is finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import { Header } from './components/Header/index'
-import { Task } from './components/Tasks/index'
+import { useState } from "react";
 
 function App() {
-
+  const [newItem, setNewItem] = useState("");
+  const [items,setItems] = useState([])
+  function addItem() {
+    if(!newItem.trim()){
+      alert("enter a item")
+      return;
+    }
+    const item = {
+      id: crypto.randomUUID(),
+      value: newItem
+    }
+    setItems(oldItems => [...oldItems,item])
+    setNewItem("");
+  }
+  function deleteItem(id){
+    const newArray = items.filter(item => item.id != id )
+    setItems(newArray);
+  }
   return (
-    <>
-    <Header/>
-    <Task/>
-    </>
-   
-  )
+    <div className="App">
+      <h1>To do app</h1>
+      <input
+        type="text"
+        placeholder="add an item"
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button onClick={() => addItem()}>Add</button>
+     {items.map(item => (
+      <li key={item.id}>{item.value} <button onClick={() => deleteItem(item.id)}>X</button></li> 
+     ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
